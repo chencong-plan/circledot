c24b4ba3460be81eef5c12a1f72a9403fbefd5c5
chencong <cong.chen@42pay.com>
2019-12-19 16:09:10 +0800

    Initial commit

---
+++ src/main/java/cc/ccoder/circledot/CircledotApplication.java
@@ -0,0 +1,13 @@
+package cc.ccoder.circledot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircledotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircledotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/ccoder/circledot/CircledotApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.ccoder.circledot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircledotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
